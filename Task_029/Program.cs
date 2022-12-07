5b6a8f9b7f18cdac6c61d36251656d08855ac0ad
SharOFF <sharov-pa@yandex.ru>
2022-12-07 21:00:21 +0300

    Task_029

---
+++ Task_029/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task_029
+// Напишите программу, которая задаёт массив из 8 элементов, запрашивает данные(элементы массива) у пользователя и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"Введите элемент {i + 1}: \n");
+  array[i] = Convert.ToInt32(Console.ReadLine());  
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
